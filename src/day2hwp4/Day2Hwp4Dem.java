1a82250103dc2d1be1f9a41a813ccd34a1d3e93d
sabamansoori <sabamansoori@10.0.0.102>
2023-04-10 16:10:37 -0400

    initial commit

---
+++ src/day2hwp4/Day2Hwp4Dem.java
@@ -0,0 +1,23 @@
+package day2hwp4;
+
+public class Day2Hwp4Dem {
+
+	
+public static void main (String args []) {
+		
+		System.out.println ("Day 2 hw 4 stash");
+		
+	}
+
+public void subtract (int a, int b) {
+	
+	System.out.println (a-b);
+
+	
+	
+	
+	
+	
+	
+	
+}}
